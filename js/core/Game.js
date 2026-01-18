18e0ca2fe03b35bcb36172ba5d3f72aed6cfc4a2
Ahmed Ehab <ahmedehab62003@gmail.com>
2026-01-18 13:00:17 +0200

    make pause menu

---
+++ js/core/Game.js
@@ -0,0 +1,13 @@
+const pauseModal = document.getElementById("pause-menu");
+
+function onPause() {
+  pauseModal.showModal();
+}
+
+function onResume() {
+  pauseModal.close();
+}
+
+function onRestart() {}
+
+function onQuit() {}
