d7ecb887c2e14308b5efbe761c3b32951337d3d4
muhanadmedhat <muhanadronaldo@yahoo.com>
2026-01-25 21:11:02 +0200

    Fixed the mummies bug in maze.js

--- js/maze/Maze.js
+++ js/maze/Maze.js
@@ -89,6 +89,7 @@ function drawMaze(maze, camera = { x: 0, y: 0 }) {
   gemPositions = [];
   //i used this to get the trap positions
   trapPositions = [];
+  mummyPositions = [];
   for (let i = 0; i < maze.length; i++) {
     for (let j = 0; j < maze[i].length; j++) {
       // i for row(y) ,j for column(x)
